e23467dd815e8d529c116004787c16f5ec4a4ee8
Klinikoz <shihabkhan2025@gmail.com>
2025-08-03 21:14:49 -0400

    fleshed out game stats to update the high score and save and read from a file

--- game_stats.py
+++ game_stats.py
@@ -1,3 +1,5 @@
+#from pathlib import Path
+import json
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -5,11 +7,34 @@ if TYPE_CHECKING:
 
 class GameStats():
 
-    def __init__(self, game):
+    def __init__(self, game: 'AlienInvasion'):
         self.game = game
         self.settings = game.settings
         self.max_score = 0
+        self.init_saved_scored()
         self.reset_stats()
+
+    def init_saved_scored(self):
+        self.path = self.settings.scores_file
+        if self.path.exists() and self.path.stat.__sizeof__() > 80:
+            contents = self.path.read_text()
+            scores = json.loads(contents)
+            self.hi_score = scores.get('hi_score', 0)
+        else:
+            self.hi_score = 0
+            self.save_scores()
+            #save the file
+
+    def save_scores(self):
+        scores = {
+            'hi_score': self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f'File Not Found: {e}')
+
 
     def reset_stats(self):
         self.ships_left = self.settings.starting_ship_count
@@ -22,15 +47,22 @@ class GameStats():
         # max_score, 
         self._update_max_score()
         # hi_score
+        self._update_hi_score()
 
     def _update_max_score(self):
         if self.score > self.max_score:
             self.max_score = self.score
         #print(f'Basic: {self.score}')
 
+    def _update_hi_score(self):
+        if self.score > self.hi_score:
+            self.hi_score = self.score
+        #print(f'Basic: {self.score}')
+
     def _update_score(self, collisions):
         for alien in collisions.values():
             self.score += self.settings.alien_points
+    
     def update_level(self):
         self.level += 1
         #print(self.level)
